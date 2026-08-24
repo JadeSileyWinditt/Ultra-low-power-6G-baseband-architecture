ebfc4d1a00cac64920c48a79b8cadaee70b24d1b
Jade Siley-Winditt <250538760+JadeSileyWinditt@users.noreply.github.com>
2026-08-24 10:29:15 +0100

    Initialize __init__.py for optim module

---
+++ python/optim/__init__.py
@@ -0,0 +1,3 @@
+"""Adaptive intensity reduction / pruning controllers."""
+from .adaptive_pruning import AdaptivePruningController, PruneDecision
+__all__ = ["AdaptivePruningController", "PruneDecision"]
